06d44e27cad049361819ea5ccdb0e947f2575e58
MadhuNimmo <mchak009@ucr.edu>
2023-11-26 19:03:54 -0800

    Support for Hulu ads

--- content.js
+++ content.js
@@ -1,25 +1,35 @@
-(function() {
-    // Checks for adds and manipulates the video or uses skip button if present
+(function () {
+    // Checks for ads and manipulates the video or uses skip button if present
     function handleVideoAd() {
         const video = document.querySelector('video');
         const adElement = document.querySelector('.video-ads.ytp-ad-module');
+        // Support for Hulu
+        const adVideo = document.querySelector('#ad-video-player');
         if (video && adElement && adElement.children.length > 0) {
-            video.muted = true;
-            video.playbackRate = 10.0;
+            muteAndSpeedUp(video, 10.0)
         }
         // Skip button seems to be acessible at initialization, if its ever present
         const skipButton = document.querySelector('.ytp-ad-skip-button, .ytp-ad-skip-button-modern');
         if (skipButton) {
             skipButton.click();
-            console.log('Used Button to Skip Add')
+            console.log('Used Button to Skip Ad');
+        }
+        if (adVideo) {
+            muteAndSpeedUp(adVideo, 15.0);
         }
     }
 
-    // Handle video adds on initialization
-    handleVideoAd();
+    function muteAndSpeedUp(videoElement, playbackRate) {
+        videoElement.muted = true;
+        videoElement.playbackRate = playbackRate;
+    }
 
-    // Handle video adds when there are content changes
-    const observer = new MutationObserver(handleVideoAd);
-    
-    observer.observe(document.body, { childList: true, subtree: true });
+    function initializeAdHandling() {
+        handleVideoAd();
+
+        const observer = new MutationObserver(handleVideoAd);
+        observer.observe(document.body, { childList: true, subtree: true });
+    }
+    // Delay before executing the script (required for Hulu)
+    setTimeout(initializeAdHandling, 2000);
 })();
